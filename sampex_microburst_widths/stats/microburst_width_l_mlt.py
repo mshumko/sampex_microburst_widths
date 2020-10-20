2626b2884756a88049286c40287db500e3db35f8
Mykhaylo Mike Shumko <msshumko@gmail.com>
2020-10-20 13:22:45 -0600

    Finished making the L-MLT plots. I have a feeling that I'll need to address the very narrow microbursts. Maybe use the adjusted R^2?

--- sampex_microburst_widths/stats/microburst_width_l_mlt.py
+++ sampex_microburst_widths/stats/microburst_width_l_mlt.py
@@ -17,6 +17,7 @@ def main():
     max_width = 0.25
     # width_bins=np.linspace(0, max_width, num=50)
     L_bins = np.linspace(2, 8.1, num=20)
+    L_labels = [2,4,6]
     MLT_bins = np.linspace(0, 24, num=40)
 
     df = pd.read_csv(pathlib.Path(config.PROJECT_DIR, 'data', catalog_name))
@@ -27,7 +28,9 @@ def main():
 
     num_microbursts_H, _, _ = np.histogram2d(df['MLT'], df['L_Shell'],
                                             bins=[MLT_bins, L_bins])
-    width_H = np.nan*np.zeros((len(MLT_bins), len(L_bins)))
+    H_q25 = np.nan*np.zeros((len(MLT_bins), len(L_bins)))
+    H_q50 = np.nan*np.zeros((len(MLT_bins), len(L_bins)))
+    H_q75 = np.nan*np.zeros((len(MLT_bins), len(L_bins)))
 
     for i, (start_MLT, end_MLT) in enumerate(zip(MLT_bins[:-1], MLT_bins[1:])):
         for j, (start_L, end_L) in enumerate(zip(L_bins[:-1], L_bins[1:])):
@@ -36,21 +39,34 @@ def main():
                 (df['L_Shell'] > start_L) &  (df['L_Shell'] < end_L)
                 ), 'fwhm']
             if df_flt.shape[0] > 50:
-                width_H[i, j] = df_flt.median()
+                H_q25[i, j] = df_flt.quantile(0.25)
+                H_q50[i, j] = df_flt.quantile(0.5)
+                H_q75[i, j] = df_flt.quantile(0.75)
 
-    fig = plt.figure(figsize=(10, 4))
-    ax = plt.subplot(121, projection='polar')
-    bx = plt.subplot(122, projection='polar')
+    fig = plt.figure(figsize=(10, 8))
+    ax = plt.subplot(221, projection='polar')
+    bx = plt.subplot(222, projection='polar')
+    cx = plt.subplot(223, projection='polar')
+    dx = plt.subplot(224, projection='polar')
 
-    d = dial_plot.Dial(ax, MLT_bins, L_bins, width_H)
-    d.draw_dial(L_labels=[2,4,6],
-                colorbar_kwargs={'label':'Median microburst width [s]'})
+
+    d = dial_plot.Dial(ax, MLT_bins, L_bins, H_q25)
+    d.draw_dial(L_labels=L_labels,
+                colorbar_kwargs={'label':'25% microburst width [s]'})
+
+    d2 = dial_plot.Dial(bx, MLT_bins, L_bins, H_q50)
+    d2.draw_dial(L_labels=L_labels,
+                colorbar_kwargs={'label':'50% microburst width [s]'})
     
-    d2 = dial_plot.Dial(bx, MLT_bins, L_bins, num_microbursts_H)
-    d2.draw_dial(L_labels=[2,4,6],
+    d3 = dial_plot.Dial(cx, MLT_bins, L_bins, H_q75)
+    d3.draw_dial(L_labels=L_labels,
+                colorbar_kwargs={'label':'75% microburst width [s]'})
+
+    d4 = dial_plot.Dial(dx, MLT_bins, L_bins, num_microbursts_H)
+    d4.draw_dial(L_labels=L_labels,
                 colorbar_kwargs={'label':'Number of microbursts'})
 
-    plt.suptitle('SAMPEX microburst width statistics')
+    plt.suptitle(f'SAMPEX microburst width statistics\ncatalog_name={catalog_name}')
     return
 
 main()
