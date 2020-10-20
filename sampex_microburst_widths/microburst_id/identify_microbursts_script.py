936da0d6d1c3a4a4d4fb0d1979918d418d5de765
Mykhaylo Mike Shumko <msshumko@gmail.com>
2020-10-20 12:12:10 -0600

    Updated the detection parameters for another detection run. This time the parameters is the O'Brien 2003 burst parameter with n100=0.1 s amd a500 = 0.5 s.

--- sampex_microburst_widths/microburst_id/identify_microbursts_script.py
+++ sampex_microburst_widths/microburst_id/identify_microbursts_script.py
@@ -2,7 +2,7 @@ from sampex_microburst_widths.microburst_id import identify_microbursts
 from sampex_microburst_widths.microburst_id import append_attitude
 
 m = identify_microbursts.Identify_SAMPEX_Microbursts(
-    baseline_width_s=1, foreground_width_s=0.1,
+    baseline_width_s=0.5, foreground_width_s=0.1,
     prominence_rel_height=0.5
     )
 try:
