61638f1ea9662ebf28081da2ee8c211d81d19010
Mykhaylo Mike Shumko <msshumko@gmail.com>
2020-10-28 14:17:52 -0600

    Added the merge_AE data program to the data pipeline.

--- sampex_microburst_widths/microburst_id/identify_microbursts_script.py
+++ sampex_microburst_widths/microburst_id/identify_microbursts_script.py
@@ -1,8 +1,10 @@
 from sampex_microburst_widths.microburst_id import identify_microbursts
 from sampex_microburst_widths.microburst_id import merge_attitude
+from sampex_microburst_widths.microburst_id import merge_ae
 
+# Identify and fit microbursts
 m = identify_microbursts.Identify_SAMPEX_Microbursts(
-    baseline_width_s=0.5, foreground_width_s=0.1,
+    baseline_width_s=2, foreground_width_s=0.1,
     prominence_rel_height=0.5
     )
 try:
@@ -10,6 +12,12 @@ try:
 finally:
     cat_path = m.save_catalog()
 
+# Merge the Attitude data
 m = merge_attitude.Merge_Attitude(cat_path)
+m.loop()
+m.save_catalog()
+
+# Merge the AE data
+m = merge_ae.Merge_AE(cat_path)
 m.loop()
 m.save_catalog()
