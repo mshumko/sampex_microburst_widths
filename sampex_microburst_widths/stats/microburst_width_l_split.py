a8128c181552d3900f3d06e1476d398ab58a21d9
Mykhaylo Mike Shumko <msshumko@gmail.com>
2020-11-03 10:22:40 -0700

    Added the 3rd catalog to gitignore because it is too big.

---
+++ sampex_microburst_widths/stats/microburst_width_l_split.py
@@ -0,0 +1,33 @@
+# Histogram the microburst widths as a function of L shell
+import pathlib
+
+import pandas as pd
+import numpy as np
+import matplotlib.pyplot as plt
+
+from sampex_microburst_widths import config
+
+
+catalog_name = 'microburst_catalog_02.csv'
+MLT_bins = {'nightside':[21, 3], 'dayside':[9, 15]}
+r2_thresh = 0.9
+max_width = 0.25
+width_bins=np.linspace(0, max_width, num=50)
+L_bins = np.linspace(3, 8, num=50)
+LL, WW = np.meshgrid(L_bins, width_bins)
+
+df = pd.read_csv(pathlib.Path(config.PROJECT_DIR, 'data', catalog_name))
+df['fwhm'] = df['fwhm'].abs()
+df = df[df.adj_r2 > r2_thresh]
+H, _, _ = np.histogram2d(df['L_Shell'], df['fwhm'], bins=[L_bins, width_bins])
+fig, ax = plt.subplots()
+im = ax.pcolormesh(LL, WW, H.T)
+fig.colorbar(im, ax=ax)
+ax.text(0.95, 0.95, "O'Brien burst parameter n_100=0.1 s, a_500=1 s"
+                    f"\nGaussian FWHM | min R^2 = {r2_thresh}"
+                    "\nno visual inspection", 
+        ha='right', va='top', transform=ax.transAxes, color='white'
+        )
+ax.set(xlabel='L_Shell', ylabel='Width [s]')
+
+plt.show()
