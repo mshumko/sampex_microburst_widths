d8a2530720d3d0e2646be8b55d7127391db0a5d5
Mykhaylo Shumko <msshumko@gmail.com>
2021-02-11 12:30:57 -0700

    Updated the microburst width comparison plots with the added A_2000 catalog.

--- sampex_microburst_widths/stats/microburst_width.py
+++ sampex_microburst_widths/stats/microburst_width.py
@@ -11,11 +11,12 @@ from sampex_microburst_widths.misc import plot_annotator_decorator
 # Catalog 0 uses A1000
 # Catalog 2 uses A500
 # Catalog 3 uses A2000
+# Catalog 4 uses A500 with a different c0 parameter guess.
 
 catalog_params = {
     'microburst_catalog_00.csv':1000,
     'microburst_catalog_02.csv':500,
-    # 'microburst_catalog_03.csv':2000,
+    'microburst_catalog_03.csv':2000,
 }
 
 @plot_annotator_decorator.annotate_plot
@@ -24,15 +25,17 @@ def main():
 
     for cat_name, background_width in catalog_params.items():
         df = pd.read_csv(pathlib.Path(config.PROJECT_DIR, 'data', cat_name))
+        df.dropna(inplace=True)
+        if hasattr(df, 'fwhm'):
+            df['fwhm'] = df['fwhm'].abs()
+            df = df[df.adj_r2 > 0.9]
 
         ax.hist(df['width_s'], bins=np.linspace(0, 0.5, num=20), histtype='step', 
                 label=r'$A_{{{0}}}$'.format(background_width))
         ax.set_title('SAMPEX-HILT | >1 MeV Microburst Widths')
         # ax.set_yscale('log')
         ax.set_xlabel('Width [s]')
-        # ax.text(0.95, 0.95, "O'Brien burst parameter\nWidth at half prominence\nno visual inspection", 
-        #         ha='right', va='top', transform=ax.transAxes
-        #         )
+        print(f'For the catalog with A_{background_width}, the median duration is {df["width_s"].median()}')
 
     plt.legend()
 main()
